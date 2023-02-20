e888c852aba1d311dd9b763de68875d11fe03d64
PILLENGDAO <51523749+rankspitter@users.noreply.github.com>
2023-02-20 20:18:53 +0700

    Update no6.s

--- no6.s
+++ no6.s
@@ -10,13 +10,11 @@ _read:
 
 
 _compare:
-        LDRB R2, [R1]
-        CMP R2, #'0'
-        BEQ _outzero
-        CMP R2, #'1'
-        BEQ _outone
-        CMP R2, #'2'
-        BEQ _outtwo
+        LDRB R2, [R1] @โหลดค่าจาก input มาเก็บไว้ที่ R2
+        CMP R2, #'1'    @เปรียบเทียบค่าที่อยู่ใน R2 กับ '1'
+        BEQ _outone     @ถ้าเท่ากับ 1 ให้ไปที่ _outone
+        CMP R2, #'2'    
+        BEQ _outtwo     
         CMP R2, #'3'
         BEQ _outtree
         CMP R2, #'4'
@@ -34,22 +32,13 @@ _compare:
         B _exit
 
 
-
-_outzero:
-        MOV R7, #4
-        MOV R0, #1
-        MOV R2,#4
-        LDR R1, =zero
-        SWI 0
-        B _exit
-
 _outone:
         MOV R7, #4
         MOV R0, #1
-        MOV R2,#4
-        LDR R1, =one
-        SWI 0
-        B _exit
+        MOV R2,#4       @จำนวน byte ที่จะเขียน
+        LDR R1, =one    @ที่อยู่ของข้อความ
+        SWI 0           @เรียกใช้งาน system call
+        B _exit         @กลับไปที่ _exit
 
 _outtwo:
         MOV R7, #4
@@ -62,7 +51,7 @@ _outtwo:
 _outtree:
         MOV R7, #4
         MOV R0, #1
-        MOV R2,#5
+        MOV R2,#4
         LDR R1, =tree
         SWI 0
         B _exit
@@ -70,7 +59,7 @@ _outtree:
 _outfour:
         MOV R7, #4
         MOV R0, #1
-        MOV R2,#5
+        MOV R2,#4
         LDR R1, =four
         SWI 0
         B _exit
@@ -78,7 +67,7 @@ _outfour:
 _outfive:
         MOV R7, #4
         MOV R0, #1
-        MOV R2,#5
+        MOV R2,#4
         LDR R1, =five
         SWI 0
         B _exit
@@ -94,7 +83,7 @@ _outsix:
 _outseven:
         MOV R7, #4
         MOV R0, #1
-        MOV R2,#5
+        MOV R2,#4
         LDR R1, =seven
         SWI 0
         B _exit
@@ -102,7 +91,7 @@ _outseven:
 _outeight:
         MOV R7, #4
         MOV R0, #1
-        MOV R2,#5
+        MOV R2,#4
         LDR R1, =eight
         SWI 0
         B _exit
@@ -110,22 +99,21 @@ _outeight:
 _outnine:
         MOV R7, #4
         MOV R0, #1
-        MOV R2,#5
+        MOV R2,#4
         LDR R1, =nine
         SWI 0
         B _exit
 
 _exit:
-	MOV R7, #1
+	MOV R7, #1      @เรียกใช้งาน system call ที่ 1
 	SWI 0
 
 
-	.data
-input: .space 1
-zero:    .ascii	"zero\n"
-one:	.ascii	"one\n"
+	.data   @ส่วนของข้อมูล
+input: .space 1  @สร้างพื้นที่เก็บข้อมูล 1 byte
+one:	.ascii	"one\n" @สร้างพื้นที่เก็บข้อความ "one\n"
 two:    .ascii  "two\n"
-tree:    .ascii  "three\n"
+tree:    .ascii  "tree\n"
 four:    .ascii  "four\n"
 five:    .ascii  "five\n"
 six:    .ascii  "six\n"
